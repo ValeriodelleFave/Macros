95c712edee17b2e12f38fc3dadbe243441173d33
Valerio delle Fave <48824674+ValeriodelleFave@users.noreply.github.com>
2022-04-27 15:21:57 +0200

    fix

--- app/src/main/java/com/example/macros/FirstFragment.java
+++ app/src/main/java/com/example/macros/FirstFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,11 +16,8 @@ public class FirstFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-
         return binding.getRoot();
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
